0ec30ae7d3878741089f089f810631ad3427da7a
zack_zheng <zack_zhengxiyun@163.com>
2022-09-19 22:02:19 +0800

    no message

--- vite-vue3-ts/src/test.ts
+++ vite-vue3-ts/src/test.ts
@@ -1,8 +1,13 @@
-interface A {
-  name: string
-}
+import type { A } from './types'
+
+// declare const enum Num {
+//   First = 0,
+//   Second = 1,
+// }
 
 export const a: A = {
   name: 'zack',
-  // age: 18,
+  age: 18,
 }
+
+export { A }
