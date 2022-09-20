16670c2f85cb22b77b6e49d14b1deec2cef70b2f
zack_zheng <zack_zhengxiyun@163.com>
2022-09-20 22:23:52 +0800

    vite-react ssr

--- vite-react/server.js
+++ vite-react/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const express = require('express')
 
 const app = express()
@@ -6,10 +7,23 @@ const { createServer: createViteServer } = require('vite')
 
 createViteServer({
   server: {
-    middlewareMode: 'html',
+    // middlewareMode: 'html',
+    middlewareMode: true,
   },
+  appType: 'custom',
 }).then((vite) => {
   app.use(vite.middlewares)
+
+  app.get('*', async (req, res) => {
+    let template = fs.readFileSync('index.html', 'utf-8')
+
+    template = await vite.transformIndexHtml(req.url, template)
+
+    const { render } = await vite.ssrLoadModule('/src/server-entry.jsx')
+    const html = await render(req.url)
+    const responseHtml = template.replace('<!--APP_HEML-->', html)
+    res.set('content-type', 'text/html').send(responseHtml)
+  })
 
   app.listen(4000)
 })
--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,31 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import { useState } from 'react'
+import { Link, Outlet, Route, Routes } from 'react-router-dom'
+// import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="dashboard" element={<Dashboard />} />
+
+      {/* Using path="*"" means "match anything", so this route
+            acts like a catch-all for URLs that we don't have explicit
+            routes for. */}
+      <Route path="*" element={<NoMatch />} />
+    </Route>
+  </Routes>
+  )
+}
+
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  )
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  )
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  )
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
       </p>
     </div>
   )
