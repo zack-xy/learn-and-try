ecb2edb1731df60ddb87882696e63da41a2977f1
zack_zheng <zack_zhengxiyun@163.com>
2022-09-21 07:10:43 +0800

    no message

--- vite-react/server.js
+++ vite-react/server.js
@@ -3,27 +3,51 @@ const express = require('express')
 
 const app = express()
 
-const { createServer: createViteServer } = require('vite')
+// 生产环境
+const template = fs.readFileSync('dist/client/index.html', 'utf-8')
 
-createViteServer({
-  server: {
-    // middlewareMode: 'html',
-    middlewareMode: true,
-  },
-  appType: 'custom',
-}).then((vite) => {
-  app.use(vite.middlewares)
+app.use(express.static('dist/client'))
 
-  app.get('*', async (req, res) => {
-    let template = fs.readFileSync('index.html', 'utf-8')
+app.get('*', async (req, res) => {
+  const render = require('./dist/server/sever-entry.mjs')
+  const context = {}
+  const html = await render(req.url, context)
 
-    template = await vite.transformIndexHtml(req.url, template)
+  if (context.url) {
+    res.redirect(301, context.url)
+    return
+  }
 
-    const { render } = await vite.ssrLoadModule('src/sever-entry.jsx')
-    const html = await render(req.url)
-    const responseHtml = template.replace('<!--APP_HEML-->', html)
-    res.set('content-type', 'text/html').send(responseHtml)
-  })
-
-  app.listen(4000)
+  const responseHtml = template.replace('<!--APP_HEML-->', html)
+  res.set('content-type', 'text/html').send(responseHtml)
 })
+app.listen(4000)
+
+// 本地开发时
+// const { createServer: createViteServer } = require('vite')
+
+// createViteServer({
+//   server: {
+//     // middlewareMode: 'html',
+//     middlewareMode: true,
+//   },
+//   appType: 'custom',
+// }).then((vite) => {
+//   app.use(vite.middlewares)
+
+//   app.get('*', async (req, res) => {
+//     let template = fs.readFileSync('index.html', 'utf-8')
+
+//     template = await vite.transformIndexHtml(req.url, template)
+
+//     const { render } = await vite.ssrLoadModule('src/sever-entry.jsx')
+//     const html = await render(req.url)
+//     const responseHtml = template.replace('<!--APP_HEML-->', html)
+//     res.set('content-type', 'text/html').send(responseHtml)
+//   })
+
+//   app.listen(4000)
+// })
+
+// 参考资源链接
+// https://github.com/remix-run/react-router/blob/dev/examples/ssr/server.js
