af85f339f1480ece0b137a7530787bdab6f17847
zack zheng <zack_zhengxiyun@163.com>
2023-01-09 09:44:04 +0800

    no message

--- jest-starter/tests/utils/sleep.test.ts
+++ jest-starter/tests/utils/sleep.test.ts
@@ -1,24 +1,29 @@
 import sleep from "@/utils/sleep";
 
-describe('sleep', () => {
-  beforeAll(() => {
+describe("sleep", () => {
+  it("可以在 1s 后再执行", async () => {
     jest.useFakeTimers();
-  })
 
-  it('可以睡眠 1000ms', async () => {
-    const callback = jest.fn();
-
-    const act = async () => {
-      await sleep(1000)
+    const act = async (callback: () => void) => {
+      await sleep(1000);
       callback();
-    }
+    };
 
-    act()
+    const mockCallback = jest.fn();
 
-    expect(callback).not.toHaveBeenCalled();
+    const promise = act(mockCallback);
 
+    // mockCallback 还未调用
+    expect(mockCallback).not.toBeCalled();
+
+    // 清算 Jest Message Queue 的回调，其中会执行 setTimeout 里的 resolve 函数
     jest.runAllTimers();
 
-    expect(callback).toHaveBeenCalledTimes(1);
-  })
-})
+    // 执行 callback 内容
+    await promise;
+
+    // mockCallback 已调用
+    expect(mockCallback).toBeCalled();
+    expect(mockCallback).toHaveBeenCalledTimes(1);
+  });
+});
