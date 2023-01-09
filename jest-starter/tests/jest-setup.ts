f3c08447999562af8da779a6a0e98d029576236a
zack zheng <zack_zhengxiyun@163.com>
2023-01-09 09:41:35 +0800

    no message

--- jest-starter/tests/jest-setup.ts
+++ jest-starter/tests/jest-setup.ts
@@ -1,20 +1,23 @@
+import mockConsole from "jest-mock-console";
+mockConsole()
+
 // node环境没有localStorage
 // 所以要自己mock实现一个
-Object.defineProperty(global, 'localStorage', {
-  value: {
-    store: {} as Record<string, string>,
-    setItem(key: string, value: string) {
-      this.store[key] = value;
-    },
-    getItem(key: string) {
-      return this.store[key];
-    },
-    removeItem(key: string) {
-      delete this.store[key];
-    },
-    clear() {
-      this.store = {}
-    }
-  },
-  configurable: true,
-})
+// Object.defineProperty(global, 'localStorage', {
+//   value: {
+//     store: {} as Record<string, string>,
+//     setItem(key: string, value: string) {
+//       this.store[key] = value;
+//     },
+//     getItem(key: string) {
+//       return this.store[key];
+//     },
+//     removeItem(key: string) {
+//       delete this.store[key];
+//     },
+//     clear() {
+//       this.store = {}
+//     }
+//   },
+//   configurable: true,
+// })
