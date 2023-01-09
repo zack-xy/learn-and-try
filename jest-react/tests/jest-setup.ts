6daa4badd08025d9fdaf316cced5aeca1f84783a
zack zheng <zack_zhengxiyun@163.com>
2023-01-09 15:09:11 +0800

    mock http

--- jest-react/tests/jest-setup.ts
+++ jest-react/tests/jest-setup.ts
@@ -1,3 +1,17 @@
 import '@testing-library/jest-dom'
-import mockConsole from "jest-mock-console";
-mockConsole()
+// import mockConsole from "jest-mock-console";
+// mockConsole()
+
+import server from "./mockServer/server";
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
